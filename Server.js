00c60bf61b6305cbcbeb5000186277ba9b6f6f16
Andrew Mueller <iglooworks1505@gmail.com>
2020-02-20 00:18:30 -0600

    ok, now I think I fixed all of the lazy mistakes

--- Server.js
+++ Server.js
@@ -13,9 +13,9 @@ app.use(express.json({ extended: false }));
 app.use("/list", require("./routes/List"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/build"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
   );
 }
 
